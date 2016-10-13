252fb46597f0430c7d26257c220d02985c82ff52
Johannes Heim <johannes.heim@rz.uni-freiburg.de>
2016-10-13 14:26:47 +0200

    new function gather messages

--- classes/class.ilPDLitfassUIHookGUI.php
+++ classes/class.ilPDLitfassUIHookGUI.php
@@ -28,51 +28,6 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 	 */
 	function getHTML($a_comp, $a_part, $a_par = array())
 	{
-		// do not show the search part of the main menu
-		// $a_par["main_menu_gui"]
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_search")
-		{
-			// $a_par["main_menu_gui"] is ilMainMenu object
-			// $a_par["main_menu_search_gui"] is ilMainMenuSearchGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::REPLACE, "html" => "");
-		}
-		
-		// add something to the main menu entries
-		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_list_entries")
-		{
-			// $a_par["main_menu_gui"] is ilMainMenu object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND, "html" =>
-				" <a class='MMInactive' href='http://www.ilias.de' target='_blank'>ILIAS Home Page</a>");
-		}
-		
-		// add something to locator
-		if ($a_comp == "Services/Locator" && $a_part == "main_locator")
-		{
-			// $a_par["locator_gui"] is ilLocatorGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND,
-				"html" => " » My Locator");
-		}
-
-		// add things to the personal desktop overview
-		if ($a_comp == "Services/PersonalDesktop" && $a_part == "left_column")
-		{
-			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::PREPEND,
-				"html" => "Prepend to left");
-		}
-
-		// add things to the personal desktop overview
-		if ($a_comp == "Services/PersonalDesktop" && $a_part == "right_column")
-		{
-			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
-			
-			return array("mode" => ilUIHookPluginGUI::APPEND,
-				"html" => "Append to right");
-		}
 
 		// add things to the personal desktop overview
 		if ($a_comp == "Services/PersonalDesktop" && $a_part == "center_column")
@@ -80,12 +35,23 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 			// $a_par["personal_desktop_gui"] is ilPersonalDesktopGUI object
 			
 			return array("mode" => ilUIHookPluginGUI::PREPEND,
-				"html" => "Prepend to center");
+				"html" => $this->getLitfassHTML());
 		}
 
 		return array("mode" => ilUIHookPluginGUI::KEEP, "html" => "");
 	}
 	
+
+	/**
+	* gather Messages
+	*
+	* @return string HTML of lifass-Block
+	*/
+	function getLitfassHTML()
+	{
+		return "huhu";
+	}
+
 	/**
 	 * Modify GUI objects, before they generate ouput
 	 *
@@ -93,7 +59,7 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 	 * @param string $a_part string that identifies the part of the UI that is handled
 	 * @param string $a_par array of parameters (depend on $a_comp and $a_part)
 	 */
-	function modifyGUI($a_comp, $a_part, $a_par = array())
+/**	function modifyGUI($a_comp, $a_part, $a_par = array())
 	{
 		// currently only implemented for $ilTabsGUI
 		
@@ -110,6 +76,6 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 			$a_par["tabs"]->addTab("PDLitfass", "PDLitass", "PDLitfass");
 		}
 	}
-
+*/  
 }
 ?>
