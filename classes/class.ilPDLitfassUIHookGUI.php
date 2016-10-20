c383c36138c2fe9e5ab374acf9c0412bc89604bf
Johannes Heim <johannes.heim@rz.uni-freiburg.de>
2016-10-20 14:52:26 +0200

    example delete

--- classes/class.ilPDLitfassUIHookGUI.php
+++ classes/class.ilPDLitfassUIHookGUI.php
@@ -54,7 +54,11 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 			
 			return array("mode" => ilUIHookPluginGUI::PREPEND,
 				"html" => $this->getLitfassHTML(ilPDLitfassConfigGUI::getcurrentID()));
-		}
+			return array("mode" => ilUIHookPluginGUI::PREPEND,
+				"html" => $this->getLitfassHTML(ilPDLitfassConfigGUI::getcurrentID()));
+	
+		
+			}
 
 
 		return array("mode" => ilUIHookPluginGUI::KEEP, "html" => "");
@@ -88,30 +92,5 @@ class ilPDLitfassUIHookGUI extends ilUIHookPluginGUI
 			return false;
 	}
 
-	/**
-	 * Modify GUI objects, before they generate ouput
-	 *
-	 * @param string $a_comp component
-	 * @param string $a_part string that identifies the part of the UI that is handled
-	 * @param string $a_par array of parameters (depend on $a_comp and $a_part)
-	 */
-/**	function modifyGUI($a_comp, $a_part, $a_par = array())
-	{
-		// currently only implemented for $ilTabsGUI
-		
-		// tabs hook
-		// note that you currently do not get information in $a_comp
-		// here. So you need to use general GET/POST information
-		// like $_GET["baseClass"], $ilCtrl->getCmdClass/getCmd
-		// to determine the context.
-		if ($a_part == "tabs")
-		{
-			// $a_par["tabs"] is ilTabsGUI object
-			
-			// add a tab (always)
-			$a_par["tabs"]->addTab("PDLitfass", "PDLitass", "PDLitfass");
-		}
-	}
-*/  
 }
 ?>
